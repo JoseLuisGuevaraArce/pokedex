65094e4d5e65310a37efc8902f8884537aa7a44c
Jose Luis Guevara <jose.guevara@communitybrands.com>
2022-01-16 00:44:17 -0400

    pokedex(1/16/2022): Deploying pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserAnimationsModule,
     PokemonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
